217c483d16a3326a2e09ca82f83566144f840344
Rie Sadohara <56049845+RieSadohara@users.noreply.github.com>
2022-09-27 16:04:33 +0900

    update

--- users/03_calc_ASA24totals_by_occasion.R
+++ users/03_calc_ASA24totals_by_occasion.R
@@ -18,28 +18,29 @@
   source("lib/specify_data_dir.R")
   source("lib/calc_ASA24totals_by_occasion.R")
 
-# Ensure your input files have no special characters that mess up loading:
+# Ensure your input files have no special characters that interfere loading:
   #  "
   #  '
   #  #
   #  &
 
-# Use dietstudy data -----------------------------------------------------------
-# Specify the directory where the data is.
-  SpecifyDataDirectory(directory.name = "eg_data/dietstudy/")
-
-  # Load the items.csv
-    items <- read.table("Items_to_use.txt", quote = "", sep = "\t", header = T)
-
-# # Load the totals.csv
-#   totals <- read.table("Totals_to_use.txt", sep = "\t", header = T)
+# # Use dietstudy data -----------------------------------------------------------
+# # Specify the directory where the data is.
+#   SpecifyDataDirectory(directory.name = "eg_data/dietstudy/")
+# 
+#   # Load the items.csv
+#     items <- read.table("Items_to_use.txt", quote = "", sep = "\t", header = T)
+# 
+# # # Load the totals.csv
+# #   totals <- read.table("Totals_to_use.txt", sep = "\t", header = T)
 
 # Use VVKAJ data. ---------------------------------------------------------
 # Specify the directory where the data is.
-  SpecifyDataDirectory(directory.name = "eg_data/VVKAJ101-105/")
+  SpecifyDataDirectory(directory.name = "eg_data/VVKAJ")
     
-    # Load your items data.
-      Items_raw <- read.csv("VVKAJ_2021-11-09_7963_Items_NoCommas.csv", sep = ",", header=T)
+# Load your items data.
+# Items_raw <- read.csv("VVKAJ_Items_NoCommas.csv", sep = ",", header=T)
+  items_f_id_s_m <- read.table("VVKAJ_Items_f_id_s_m.txt", sep = "\t", header=T)
   
  
 # # Load your metadata if you have one. 
@@ -47,7 +48,7 @@
 #   metadata_2 <- read.csv("Food_map_txt_Metadata_2.csv", header=T)
 
 # Come back to the main directory
-  setwd(main.wd)  
+  # setwd(main_wd)  
 
 # ========================================================================================
 # Generate sum of foods consumed by each user, by day, and by occasion
@@ -55,24 +56,36 @@
   # with occasion names in words.
 # ======================================================================================== 
 # Use VVKAJ data. ---------------------------------------------------------
-    SumByOccasion(items.data=Items_raw, User.Name='UserName', 
+# Calculate sum of nutrients and food categories of items by user and by occasion;
+# output will be saved as: Items_by_User_Occ.  
+  SumByOccasion(items.data=items_f_id_s_m, User.Name='UserName', 
                   Recall.No='RecallNo',   Occ.No='Occ_No')
-    
-    AddOccNames(items.data=Items_raw, User.Name='UserName', 
+  
+# Take a look at the first 10 rows of the first 6 columns.
+# Items are summed by occasion number for each participant's RecallNo. 
+  Items_by_User_Occ[1:10, 1:6]
+
+# Use Items_by_User_Occ and add occasion names (breakfast, snacks etc.) to it. 
+# output will be saved as: Sum_by_User_Day_Occ.
+  AddOccNames(items.data=items_f_id_s_m, User.Name='UserName', 
                 Recall.No='RecallNo', Occ.No='Occ_No', Occ.Name='Occ_Name'  )
 
-# Use diet_study data. -----------------------------------------------------
-    SumByOccasion(items.data=items, User.Name='UserName', 
-                  Recall.No='RecordDayNo',   Occ.No='Occ_No')
-    
-    AddOccNames(items.data=items, User.Name='UserName', 
-                Recall.No='RecordDayNo', Occ.No='Occ_No', Occ.Name='Occ_Name')
+# Take a look at the first 6 rows. Spelt-out occasion names corresponding to occasion name 
+# have been added at the end. 
+  head(Sum_by_User_Day_Occ)
+  
+# # Use diet_study data. -----------------------------------------------------
+#     SumByOccasion(items.data=items, User.Name='UserName', 
+#                   Recall.No='RecordDayNo',   Occ.No='Occ_No')
+#     
+#     AddOccNames(items.data=items, User.Name='UserName', 
+#                 Recall.No='RecordDayNo', Occ.No='Occ_No', Occ.Name='Occ_Name')
 
 # ---------------------------------------------------------------------------------------------------------------
-# Save as a csv file.
-  write.csv(Sum_by_User_Day_Occ, 'dietstudy_Sum_by_User_Day_Occ.csv')
+# Save Sum_by_User_Day_Occ as a txt file.
+  write.table(Sum_by_User_Day_Occ, 'VVKAJ_Sum_by_User_Day_Occ.txt', sep="\t", row.names=F, quote=F)
 # This will be useful if a researcher wants to look at the sum of each eating occasion 
-# per participant. (Because Totals file sums all the occasions in one day.)
+# per participant. (Because Totals file sums all the occasions as one day)
 
 
 
